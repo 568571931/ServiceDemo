75203127edb73c610adea215e1633d04eab095ff
568571931 <568571931@qq.com>
2015-12-15 14:44:25 +0800

    我的学习之service

---
+++ src/com/example/servicedemo_v4/MainActivity.java
@@ -0,0 +1,98 @@
+package com.example.servicedemo_v4;
+
+import com.example.servicedemo_v4.Myservice_v4.MyBinder_v4;
+
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends Activity
+{
+
+	private ServiceConnection conn = new ServiceConnection()
+	{
+
+		@Override
+		public void onServiceDisconnected(ComponentName name)
+		{
+			Log.d("", "MainActivity-onServiceDisconnected");
+			isBinder = false;
+			if (binder_v4 != null)
+				binder_v4 = null;
+
+		}
+
+		@Override
+		public void onServiceConnected(ComponentName name, IBinder service)
+		{
+			Log.d("", "MainActivity-onServiceConnected");
+			isBinder = true;
+			binder_v4 = (MyBinder_v4) service;
+			binder_v4.setMessenger(messenger);
+			// binder_v4.startMyT();
+		}
+	};
+	private boolean isBinder;
+	private Intent service;
+	private Myservice_v4.MyBinder_v4 binder_v4 = null;
+	private Messenger messenger;
+	private MyHanler mHandler;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		service = new Intent(this, Myservice_v4.class);
+		mHandler = new MyHanler();
+		messenger = new Messenger(mHandler);
+		startService(service);
+		bindService(service, conn, BIND_AUTO_CREATE);
+	}
+
+	@Override
+	protected void onDestroy()
+	{
+		super.onDestroy();
+		if (isBinder)
+		{
+			unbindService(conn);
+		}
+	}
+
+	@Override
+	protected void onStop()
+	{
+		super.onStop();
+
+	}
+
+	public static final int NUMONE = 1;
+
+	class MyHanler extends Handler
+	{
+
+		@Override
+		public void handleMessage(Message msg)
+		{
+			switch (msg.what)
+			{
+			case NUMONE:
+				Log.d("", "÷¥––¡Àhandler");
+				break;
+
+			default:
+				super.handleMessage(msg);
+				break;
+			}
+		}
+	}
+}
---
+++ src/com/example/servicedemo_v4/Myservice_v4.java
@@ -0,0 +1,99 @@
+package com.example.servicedemo_v4;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
+import android.util.Log;
+
+public class Myservice_v4 extends Service
+{
+	Messenger mMessenger;
+	private MyBinder_v4 binder_v4;
+	private boolean isResume = true;
+
+	@Override
+	public IBinder onBind(Intent intent)
+	{
+		Log.d("", "Myservice_v4-onBind");
+		return binder_v4;
+	}
+
+	@Override
+	public void onCreate()
+	{
+		super.onCreate();
+		Log.d("", "Myservice_v4-onCreate");
+		binder_v4 = new MyBinder_v4();
+		thread.start();
+	}
+
+	class MyBinder_v4 extends Binder
+	{
+		public void setMessenger(Messenger messenger)
+		{
+			mMessenger = messenger;
+		}
+
+//		public void startMyT()
+//		{
+//			thread.start();
+//		}
+
+		public void setFlag(boolean b)
+		{
+			isResume = b;
+		}
+
+	}
+
+	Thread thread = new Thread(new Runnable()
+	{
+
+		@Override
+		public void run()
+		{
+
+			while (isResume)
+			{
+				Log.w("", "循环体");
+
+				sendObj(MainActivity.NUMONE, null);
+
+				try
+				{
+					Thread.sleep(3000);
+				} catch (InterruptedException e)
+				{
+					e.printStackTrace();
+					Log.e("", e.toString());
+				}
+			}
+		}
+
+		private void sendObj(int numone, Object object)
+		{
+			// TODO Auto-generated method stub
+			if (mMessenger != null)
+			{
+				try
+				{
+					Log.i("", "非空");
+					mMessenger.send(Message.obtain(null, numone, null));
+				} catch (RemoteException e)
+				{
+					Log.e("error", e.toString());
+					mMessenger = null;
+					e.printStackTrace();
+				}
+			} else
+			{
+				Log.i("", "为空");
+			}
+
+		}
+	});
+}
